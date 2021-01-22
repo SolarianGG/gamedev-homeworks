c8c76689868445a07f93803f7469463cc7ead49a
matvey <matvey@pop-os.localdomain>
2021-01-22 22:26:51 +0300

    Add first homework

---
+++ dz-01-1/main.cxx
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <string_view>
+
+#include <chrono>
+#include <thread>
+
+static std::string_view get_user_name(char**);
+
+int main(int /*argc*/, char* /*argv*/[], char** env)
+{
+    using namespace std;
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    string_view user_name = get_user_name(env);
+
+    string_view output_phrase("hello,");
+
+    cout << output_phrase << " " << user_name << endl;
+
+    bool is_good = cout.good();
+
+    int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
+    return result;
+}
+// clang-format off
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// clang-format on
+static char** get_env_end(char** env)
+{
+    while (*env)
+    {
+        ++env;
+    }
+
+    return env;
+}
+
+static bool start_with(std::string_view str, std::string_view start)
+{
+    return str.size() >= start.size() &&
+           std::equal(begin(start), end(start), begin(str));
+}
+
+static std::string_view get_value(std::string_view key_value)
+{
+    auto end_it = end(key_value);
+    auto it     = std::find(begin(key_value), end_it, '=');
+    if (it == end_it)
+    {
+        return "";
+    }
+    ++it; // skip '='
+    if (it == end_it)
+    {
+        return "";
+    }
+    return { it, static_cast<size_t>(end_it - it) };
+}
+
+static std::string_view get_user_name(char** env)
+{
+    using namespace std;
+    using namespace std::placeholders;
+
+    char** env_end = get_env_end(env);
+    auto   it      = find_if(env, env_end, bind(start_with, _1, "USER="));
+
+    return it != env_end ? get_value(*it) : "unknown";
+}
---
+++ dz-01-2/hello_bin/src/main.cxx
@@ -0,0 +1,16 @@
+#include <cstdlib>
+
+#include <hello_lib.hxx>
+
+int main(int /*argc*/, char* /*argv*/[], char* /*argv_env*/[])
+{
+    const char*      user_env_var = std::getenv("USER");
+    std::string_view user         = user_env_var != nullptr
+                                ? user_env_var
+                                : "USER - environment variable not found";
+
+    bool is_good = greetings(user);
+
+    int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
+    return result;
+}
